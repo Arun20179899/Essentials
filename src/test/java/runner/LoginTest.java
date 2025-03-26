1b25aaebf97f5f1b9ebb2dd3b829c121fb86dbd8
Arun Kumar A S <arunkumar@selfstoragemanager.com>
2025-03-26 07:08:06 -0700

    updated LoginTest to provide dynamic testcase name

--- src/test/java/runner/LoginTest.java
+++ src/test/java/runner/LoginTest.java
@@ -4,11 +4,15 @@ import workbook.DataDriven;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class LoginTest {
     public static void main(String[] args) throws IOException {
+        Scanner sc =  new Scanner(System.in);
+        System.out.println("Enter the Test Case Name");
+        String testCaseName = sc.next();
         DataDriven data = new DataDriven();
-        ArrayList<String> value = data.getData("Orders");
+        ArrayList<String> value = data.getData(testCaseName);
 //        System.out.println(value.get(2));
         System.out.println(value);
     }
